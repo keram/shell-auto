d6fa3b3aba17a0bd5975dcd205669d34e4de54b0
Marek L <nospam.keram@gmail.com>
2020-02-01 15:30:24 +0000

    Cleanup run_report

--- run_report.sh
+++ run_report.sh
@@ -9,10 +9,17 @@ IFS=$'\n\t'
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)" || return
 echo "$(pwd)"
 
+# Load default env config
+source .env
+
+# Load default env config
+source lib/report_helpers.sh
+
+# Sanity checks on passed config file
 if [ ! -r "$1" ]; then
   error_msg="Error: $1 is not readable file."
-  echo `date` "$error_msg" >> error.log
-  echo "$error_msg"
+
+  log_error_with_tee "$error_msg"
   exit 1
 fi
 
@@ -20,36 +27,43 @@ if [[ "$1" =~ .*\.report\.cfg ]]; then
   config_file=$1
 else
   error_msg="Error: $1 is not a report config file."
-  echo `date` "$error_msg" >> error.log
-  echo "$error_msg"
+  log_error_with_tee "$error_msg"
   exit 1
 fi
 
+# Load script specific config and override defaults
 # TODO: replace source config file with parsing
 source "$config_file"
 
-config_options=( EMAIL_SUBJECT EMAIL_FROM EMAIL_RECIPIENTS )
+
+# Test that all necessary variables are set
+# and also check that global options are still present
+config_options=( EMAIL_SUBJECT EMAIL_FROM EMAIL_RECIPIENTS
+                 EXPIRE_TIME_IN_SECONDS EXPIRE_TIME_HUMAN
+                 SCRIPT_PATH
+                 LOG_FILE )
 
 for opt_name in ${config_options[@]}
 do
   if [ -z ${!opt_name} ]; then
     error_msg="Error: $1 Missing $opt_name definition or is empty."
-    echo `date` "$error_msg" >> error.log
-    echo "$error_msg"
+    log_error_with_tee "$error_msg"
     exit 1
   fi
 done
 
-# # if [[ ${#EMAIL_SUBJECT} = 0 ]]; then
-# if [ -z $EMAIL_SUBJECT ]; then
-#   error_msg="Error: $1 Missing configuration definition."
-#   echo `date` "$error_msg" >> error.log
-#   echo "$error_msg"
-#   exit 1
-# fi
+# Unsure script_path is readable file
+if [ ! -r "$SCRIPT_PATH" ]; then
+  error_msg="Error: $SCRIPT_PATH is not readable file."
+  log_error_with_tee "$error_msg"
+  exit 1
+fi
 
+script_name=$(basename "$config_file")
 
-# log_state $script_path 'initialised'
+echo $script_name
+# log_info "$script_name initialised"
+log_info_with_tee "$script_name initialised"
 # process_report $script_path
 # log_state $script_path 'generated'
 # upload_report $report_path
