a87ab1e629b08f025ea0a4d82ac08e6b9768ee1c
Marek L <nospam.keram@gmail.com>
2020-02-02 16:34:13 +0000

    Cleanup and document

--- lib/report_helpers.sh
+++ lib/report_helpers.sh
@@ -30,6 +30,7 @@ function save_email {
 function send_email {
   local email_path=$1
   local email_recipients=$2
+
   cat $email_path | msmtp -a reports "$email_recipients"
 }
 
@@ -37,17 +38,18 @@ function build_email_body {
   local download_url=$1
   local expire_in=$2
   local email_body=$(<report_email_body_template.txt)
+
   printf "$email_body" "$download_url" "$expire_in"
 }
 
 function generate_report {
   local script_path="$1"
   local report_path="$2"
-  # echo "$DATABASE_READONLY_URL -f $script_path > $report_path"
+
   # for some reason -f does not work on win
   # and uname test for eshell in emacs
   # TODO cleanup
-  if [ "$WINDOWS_OS" = "1" ] || [ $(uname) = "MINGW64_NT-10.0" ]; then
+  if [ $(uname) = "MINGW64_NT-10.0" ]; then
     psql "$DATABASE_URL" < "$script_path" > "$report_path"
   else
     psql "$DATABASE_URL" -f "$script_path" > "$report_path"
@@ -66,16 +68,27 @@ function build_report_path {
 
 function build_bucket_report_path {
   local report_path=$1
-  echo "s3://$REPORTS_S3_BUCKET/$(date +"%m-%d-%Y")/$(basename -- "$1")"
+
+  echo "s3://$REPORTS_S3_BUCKET/$(date +"%m-%d-%Y")/$(basename -- "$report_path")"
 }
 
+# TODO: trap aws failure and log to file
 function upload_report {
   local report_path=$1
   local bucket_report_path=$2
 
+  # From aws help:
+  # --cli-read-timeout (int)
+  # The  maximum socket read time in seconds. If the value is set to 0, the
+  # socket read will be blocking and not timeout.
+  # --cli-connect-timeout (int)
+  # The maximum socket connect time in seconds. If the value is set  to  0,
+  # the socket connect will be blocking and not timeout.
+  # --cli-read-timeout 1 --cli-connect-timeout 1
   aws s3 cp $report_path $bucket_report_path
 }
 
+# TODO: trap aws failure and log to file
 function generate_download_url {
   local bucket_report_path=$1
   local expire_time_in_seconds=$2
@@ -86,12 +99,14 @@ function generate_download_url {
 function log_info {
   local message=$1
   local type='INFO'
+
   log_message "$message" "$type"
 }
 
 function log_error {
   local message=$1
   local type='ERROR'
+
   log_message "$message" "$type"
 }
 
