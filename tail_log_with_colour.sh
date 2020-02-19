c87bb08f7a3081b55d3076d06d12dd82ffa09a4b
Marek L <nospam.keram@gmail.com>
2020-02-19 23:13:19 +0000

    cd to shell-auto dir for tail script

--- tail_log_with_colour.sh
+++ tail_log_with_colour.sh
@@ -1,2 +1,5 @@
 # tail -f reports.log | awk '/ERROR/ { $0 = "\033[29m" $0 "\033[39m" }; 1'
+# Set working directory to dir of this file
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)" || return
+
 tail -f reports.log | awk '/ERROR/ { $0 = "\033[31m" $0 "\033[39m" };/Email Sent/ { $0 = "\033[32m" $0 "\033[39m" }; 1'
